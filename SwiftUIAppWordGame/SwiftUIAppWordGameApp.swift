6c5aa84aa6c3f7d43a75a33558a652b986be8e44
Aleksey <aay92@yandex.ru>
2023-08-19 13:15:33 +0400

    Initial Commit

---
+++ SwiftUIAppWordGame/SwiftUIAppWordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppWordGameApp.swift
+//  SwiftUIAppWordGame
+//
+//  Created by Aleksey Alyonin on 19.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppWordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
